1b418e4d069fa7e3135d5f0d0c50b368f28599b1
osaydayoub <osaydayoub@gmail.com>
2024-03-17 01:45:36 +0200

    protect clientRoutes

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,11 +1,17 @@
 import STATUS_CODE from "../constants/statusCode.js";
 import Client from "../models/clientModel.js";
+import User from "../models/userModel.js";
 
 // @des      Get all the clients
 // @route    GET /api/v1/coach/clients
 // @access   Public
 export const getAllClients = async (req, res, next) => {
   try {
+    const user = await User.findById(req.user.id);
+    if (!user.isAdmin) {
+      res.status(STATUS_CODE.UNAUTHORIZED);
+      throw new Error("Not authorized");
+    }
     const clients = await Client.find({}).populate("workouts");
     res.status(STATUS_CODE.OK).send(clients);
   } catch (error) {
@@ -18,6 +24,11 @@ export const getAllClients = async (req, res, next) => {
 // @access   Public
 export const getClientById = async (req, res, next) => {
   try {
+    const user = await User.findById(req.user.id);
+    if (!user.isAdmin && user.client.toString() !== req.params.id) {
+      res.status(STATUS_CODE.UNAUTHORIZED);
+      throw new Error("Not authorized");
+    }
     const client = await Client.findById(req.params.id).populate("workouts");
     if (!client) {
       res.status(STATUS_CODE.NOT_FOUND);
@@ -80,6 +91,11 @@ export const assignPackage = async (req, res, next) => {
   const { numberOfWorkouts, totalCost, paidAmount, caloricIntake } = req.body;
   const { id } = req.params;
   try {
+    const user = await User.findById(req.user.id);
+    if (!user.isAdmin) {
+      res.status(STATUS_CODE.UNAUTHORIZED);
+      throw new Error("Not authorized");
+    }
     if (
       isNaN(numberOfWorkouts) ||
       isNaN(totalCost) ||
@@ -125,6 +141,11 @@ export const addDailyTracking = async (req, res, next) => {
   const { id } = req.params;
 
   try {
+    const user = await User.findById(req.user.id);
+    if (user.client.toString() !== req.params.id) {
+      res.status(STATUS_CODE.UNAUTHORIZED);
+      throw new Error("Not authorized");
+    }
     if (
       isNaN(new Date(date)) ||
       isNaN(calories) ||
@@ -135,18 +156,18 @@ export const addDailyTracking = async (req, res, next) => {
       throw new Error("Please include all fields");
     }
 
-    const user = await Client.findById(id);
+    const client = await Client.findById(id);
 
     let index = -1;
-    let updatedUser;
-    if (user) {
-      index = user.dailyTracking.findIndex((track) => {
+    let updatedClient;
+    if (client) {
+      index = client.dailyTracking.findIndex((track) => {
         return isSameDay(new Date(track.date), new Date(date));
       });
 
       if (index != -1) {
         //update
-        updatedUser = await Client.findByIdAndUpdate(
+        updatedClient = await Client.findByIdAndUpdate(
           id,
           {
             $set: {
@@ -164,7 +185,7 @@ export const addDailyTracking = async (req, res, next) => {
         );
       } else {
         //push
-        updatedUser = await Client.findByIdAndUpdate(
+        updatedClient = await Client.findByIdAndUpdate(
           id,
           {
             $push: {
@@ -186,7 +207,7 @@ export const addDailyTracking = async (req, res, next) => {
       throw new Error("No such client in the db");
     }
 
-    res.send(updatedUser);
+    res.send(updatedClient);
     // updatedUser
   } catch (error) {
     next(error);
@@ -201,14 +222,19 @@ export const getDailyTracking = async (req, res, next) => {
   const { date } = req.body;
   const { id } = req.params;
   try {
+    const user = await User.findById(req.user.id);
+    if (!user.isAdmin && user.client.toString() !== req.params.id) {
+      res.status(STATUS_CODE.UNAUTHORIZED);
+      throw new Error("Not authorized");
+    }
     if (isNaN(new Date(date))) {
       res.status(STATUS_CODE.BAD_REQUEST);
       throw new Error("Please include date");
     }
-    const user = await Client.findById(id);
+    const client = await Client.findById(id);
     let track;
-    if (user) {
-      track = user.dailyTracking.find((track) => {
+    if (client) {
+      track = client.dailyTracking.find((track) => {
         return isSameDay(new Date(track.date), new Date(date));
       });
     } else {
@@ -217,7 +243,7 @@ export const getDailyTracking = async (req, res, next) => {
     }
     if (!track) {
       res.status(STATUS_CODE.NOT_FOUND);
-      throw new Error("No such client in the db");
+      throw new Error("No available track for this day in the database");
     }
     res.send(track);
   } catch (error) {
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {
-    addDailyTracking,
+  addDailyTracking,
   assignPackage,
   createClient,
   getAllClients,
@@ -9,11 +9,13 @@ import {
 } from "../controllers/clientController.js";
 const router = express.Router();
 
+//
+import { protect } from "../middlewares/authMiddleware.js";
 // Route to get all clients
-router.get("/", getAllClients);
+router.get("/", protect, getAllClients);
 
 //Route to get a single client by ID
-router.get("/:id", getClientById);
+router.get("/:id", protect, getClientById);
 
 // Route to create a new client
 router.post("/", createClient);
@@ -22,13 +24,13 @@ router.post("/", createClient);
 // router.put("/:id", updateClient);
 
 // Route to Assign a package to a client with id
-router.put("/assignPackage/:id", assignPackage);
+router.put("/assignPackage/:id", protect, assignPackage);
 
 // Route to add a dailyTracking to client with id
-router.put("/dailyTracking/:id", addDailyTracking);
+router.put("/dailyTracking/:id", protect, addDailyTracking);
 
 // Route to get a dailyTracking of a client with id on a specific date.
-router.get("/dailyTracking/:id", getDailyTracking);
+router.get("/dailyTracking/:id", protect, getDailyTracking);
 
 // // Route to add a weeklyTracking to client with id
 // router.put("/addWeeklyTracking/:id", addWeeklyTracking);
